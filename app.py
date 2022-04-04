c3b5f2dc4ca0ab0056910f0fcd2c4539bd18123e
NSILVADE <91691102+NSILVADE@users.noreply.github.com>
2022-04-04 18:36:53 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from multiapp import MultiApp
-from apps import home, train
+from apps import home, home2
 
 #configuration of the page
 st.set_page_config(layout="wide")
@@ -8,7 +8,7 @@ st.set_page_config(layout="wide")
 app = MultiApp()
 
 app.add_app("Home", home.app)
-app.add_app("Base de Teste", train.app)
+app.add_app("Base de Teste", home2.app)
 
 # The main app
 app.run()
