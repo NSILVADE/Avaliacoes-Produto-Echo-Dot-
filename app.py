639f875aa1139b90aba09a43aaadfc159cee69a0
NSILVADE <91691102+NSILVADE@users.noreply.github.com>
2022-04-04 18:49:38 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 import streamlit as st
 from multiapp import MultiApp
 from apps import home, train
+from PIL import  Image
 
 #configuration of the page
 st.set_page_config(layout="wide")
 
 app = MultiApp()
+
+
+display = Image.open('echodot.png')
+display = np.array(display)
+col1, col2 = st.beta_columns(2)
+col1.image(display, width = 400)
+col2.title("Echo Dot - 4 Geração - Amazon")
 
 app.add_app("Home", home.app)
 app.add_app("Base de Teste", train.app)
