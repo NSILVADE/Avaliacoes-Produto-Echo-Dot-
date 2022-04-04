d2444cbbacbc28c36d30916d2a6d2fb9941b42fb
NSILVADE <91691102+NSILVADE@users.noreply.github.com>
2022-04-04 19:02:58 -0300

    Update multiapp.py

--- multiapp.py
+++ multiapp.py
@@ -15,7 +15,7 @@ class MultiApp:
         """Constructor class to generate a list which will store all our applications as an instance variable."""
         self.pages = []
     
-    def add_page(self, title, func) -> None: 
+    def add_app(self, title, func) -> None: 
         """Class Method to Add pages to the project
 
         Args:
